e241c53ced61b2f8239fd65035998a3a72828fb6
aryanshoh <aiskandar629@gmail.com>
2022-08-02 01:44:08 +0500

    Set a logic to greetings button, so "Hello World" label appears and disappears when user push the button and the button changes to "Show greetings" and "Hide greetings".

--- HelloWorldApp/ViewController.swift
+++ HelloWorldApp/ViewController.swift
@@ -9,11 +9,26 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet var greetingLabel: UILabel!
+    @IBOutlet var greetingButton: UIButton!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        greetingLabel.isHidden = true
+        greetingButton.layer.cornerRadius = 10
     }
-
+    @IBAction func greetingButtonPressed() {
+        greetingLabel.isHidden.toggle()
+        
+        greetingButton.setTitle(
+            greetingLabel.isHidden ? "Show greetings" : "Hide greetings",
+            for: .normal
+        )
+        
+    }
+    
+    
 
 }
 
